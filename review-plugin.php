f64cea1ace9d1a1875e310dadbdff95e780f8f1a
yeshwanthreddygoli <59642500+yeshwanthreddygoli@users.noreply.github.com>
2020-01-23 14:56:45 +1000

    added discription on plugin
    added the plugin discription aurthor version

---
+++ review-plugin.php
@@ -0,0 +1,13 @@
+/**
+ *
+ * Plugin Name:       Review Plugin
+ * Plugin URI:       
+ * Description:       This Plugin is used to review the products.
+ * Version:           1.0
+ * Author:            Yeshwanth Reddy Goli
+ * Author URI:        
+ * Text Domain:       Product-review
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
